4e0cab80edc4f41491611f33280ffe794c485999
gbz3 <gebizo3@gmail.com>
2018-08-04 09:37:46 +0900

    init

---
+++ try_mina_sshd/src/main/java/com/github/gbz3/try_mina_sshd/App.java
@@ -0,0 +1,13 @@
+package com.github.gbz3.try_mina_sshd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
